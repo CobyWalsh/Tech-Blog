8efed0687b9257b19e1c9b6437f1b6c3824114de
coby walsh <cobywalsh7@yahoo.com>
2023-07-25 16:43:23 -0600

    added blog post and comment routes to index.js file in controllers/api folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
+const blogPostRoutes = require('./blogPostRoutes');
+const commentRoutes = require('./commentRoutes');
+const userRoutes = require('./userroutes');
 
+router.use('/blogPost', blogPostRoutes);
+router.use('/comment', commentRoutes);
 router.use('/users', userRoutes);
 
 module.exports = router;
