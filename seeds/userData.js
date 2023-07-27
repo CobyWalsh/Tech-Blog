bd7dd6f4d8aecf065cbc09e956aebd5777b60b14
coby walsh <cobywalsh7@yahoo.com>
2023-07-27 10:32:35 -0600

    added test infor to userData.js

--- seeds/userData.js
+++ seeds/userData.js
@@ -1,7 +1,12 @@
 [
     {
-      "name": "",
-      "email": "",
-      "password": ""
+      "name": "test",
+      "email": "test@email.com",
+      "password": "test12345"
+    },
+    {
+      "name": "test2",
+      "email": "test2@email.com",
+      "password": "test212345"
     }
 ]
